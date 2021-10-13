f044753606e63c088a9503f91ae47cccd124aa47
caigenkan <cai_genkan@outlook.com>
2021-10-13 23:44:51 +0900

    delete object's script functions and unnecessary assets

--- HycFrame2D/FuncsRegister.cpp
+++ HycFrame2D/FuncsRegister.cpp
@@ -9,45 +9,7 @@
 
 #include "ObjectFactory.h"
 
-#include "..\TempBtnCmd.h"
-#include "..\TitleButton.h"
-#include "..\ModeSelectButton.h"
-#include "..\BowLoadingFade.h"
-#include "..\BowAction.h"
-#include "..\ArrowAction.h"
-#include "..\AimPointAction.h"
-#include "..\SunsManager.h"
-#include "..\FieldAction.h"
-#include "..\CloudAction.h"
-#include "..\ResultLoadingFade.h"
-#include "..\ResultSelectInput.h"
-#include "..\ExtraLoadingFade.h"
-#include "..\ExtraPlayerInput.h"
-#include "..\ExtraSunManager.h"
-#include "..\ExtraLife.h"
-#include "..\ExtraGods.h"
-#include "..\ManualAction.h"
-#include "..\StoryAction.h"
-
 void RegisterAllFuncs(ObjectFactory* _factory)
 {
-    RegisterTempBtnCmd(_factory);
-    RegisterTitleButton(_factory);
-    RegisterModeSelectButton(_factory);
-    RegisterBowLoadingFade(_factory);
-    RegisterBowAction(_factory);
-    RegisterArrowAction(_factory);
-    RegisterAimPointAction(_factory);
-    RegisterSunsManager(_factory);
-    RegisterFieldAction(_factory);
-    RegisterCloudAction(_factory);
-    RegisterResultLoadingFade(_factory);
-    RegisterResultInput(_factory);
-    RegisterExtraLoadingFade(_factory);
-    RegisterExtraPlayerInput(_factory);
-    RegisterExtraSunsManager(_factory);
-    RegisterExtraLife(_factory);
-    RegisterExtraGodsManager(_factory);
-    RegisterManualAction(_factory);
-    RegisterStoryAction(_factory);
+
 }
--- HycFrame2D/ResultSelectInput.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "Ui_all.h"
-
-void RegisterResultInput(ObjectFactory* _factory);
-
-void ResultSelectInput(UInputComponent* _uic, float _deltatime);
